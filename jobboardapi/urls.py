c7d6f51d039aa1e8f152996d34a09985a7489321
chidera6 <dictasteil@gmail.com>
2023-03-25 12:02:04 +0100

    feat: added authentication/permission

--- jobboardapi/urls.py
+++ jobboardapi/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('',include('applicantDRF.urls')),
     path('',include('employerDRF.urls')),
+    #path('auth/',include('djoser.urls')),
+    #path('auth/',include('djoser.urls.authtoken')),
+    path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
+    path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh')
 ]
