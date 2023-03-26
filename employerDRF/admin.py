e471577f2afc007b4a5a721a0f2c8af184a68a80
chidera6 <dictasteil@gmail.com>
2023-03-26 14:53:20 +0100

    feat:registered model in the admin file

--- employerDRF/admin.py
+++ employerDRF/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-#admin.site.register('employerDRF')
-#admin.site.register('applicantDRF')
+admin.site.register('EmployerProfile')
+admin.site.register('PostJobs')
