8651197467794d7b6dfd82753f6e42f124716d00
chidera6 <dictasteil@gmail.com>
2023-04-21 12:22:19 +0100

    fix:modified applicants view

--- applicantDRF/views.py
+++ applicantDRF/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import ApplicantProfile
+from .serializer import ApplicantProfileSerializer
+from rest_framework import generics,status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ApplicantProfileView(generics.ListCreateAPIView):
+    queryset = ApplicantProfile.objects.all()
+    serializer_class = ApplicantProfileSerializer
+    ordering_fields = ['location','skills']
+
+    def perform_create(self,serializer):
+        serializer.save(user = self.request.user)
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+class ApplicantProfileSingleView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ApplicantProfile.objects.all()
+    serializer_class = ApplicantProfileSerializer
+
+#search candidates based on location,based on skills,based on experience level
