3f7674fdee7dfd762ec13dbe7c19d0699ebd7e00
chidera6 <dictasteil@gmail.com>
2023-04-21 03:49:45 +0100

    feat:created the serializer

--- employerDRF/serializer.py
+++ employerDRF/serializer.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
-from .models import PostJobs,EmployerProfile
+from .models import PostJobs,EmployerProfile,CustomUser
+from djoser.serializers import UserCreateSerializer
 
+class UserRegistrationSerializer(UserCreateSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id',"username", 'first_name','last_name', 'email', 'user_type',"password",]
 
 class EmployerProfileSerializer(serializers.ModelSerializer):
+    user = UserRegistrationSerializer(many=True,read_only=True)
     class Meta:
         model = EmployerProfile
-        fields = ["name","description","location","company","telephone","date_created"]
-
+        fields = ["id","description","location","company","telephone","date_created","user"]
 
 class PostJobSerializer(serializers.ModelSerializer):
+    employer_profile = EmployerProfileSerializer(many=True,read_only=True)
     class Meta:
         model = PostJobs
-        fields = ["description","job_location","experience_level","date_created","date_updated",'employer_profile']
-
+        fields = ["id","description","job_location","experience_level","date_created","date_updated",'employer_profile']
 
