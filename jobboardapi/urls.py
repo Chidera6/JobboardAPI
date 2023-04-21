2900c2eb24ed4f90409b1e98050b6227c432bad1
chidera6 <dictasteil@gmail.com>
2023-04-21 03:53:54 +0100

    feat:added djoser urls and swagger endpoints

--- jobboardapi/urls.py
+++ jobboardapi/urls.py
@@ -1,34 +1,39 @@
-"""jobboardapi URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
-from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
+from django.urls import path,include,re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf.urls.static import static
+from django.conf import settings
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Job Board API",
+      default_version='v1',
+      description="Job Board API Documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+"""
+{
+    "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY4MTk5NjkyMywianRpIjoiZjZiNjc5MjhjNTRjNGM0ZjkyMjU4MjFkYzQ5NTllZjUiLCJ1c2VyX2lkIjoxfQ.P5OkgzaijKfivUH1zghjyLvrJ-kNmrf7M4E1pTGSYQk",
+    "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgxOTEwODIzLCJqdGkiOiJkZTNkOGY3ZDc1M2Q0NGI4OTlmNjAzZGU3ZTcyOGIwZCIsInVzZXJfaWQiOjF9.5ALFnL0Ui8oGDivVgNawTXxSbxENhEOLrF3eStioV8I"
+}
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('',include('applicantDRF.urls')),
+    path('api/v1/',include('applicantDRF.urls')),
     path('api/v1/',include('employerDRF.urls')),
-    #path('auth/',include('djoser.urls')),
-    #path('auth/',include('djoser.urls.authtoken')),
-    path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("api/v1/dj-rest-auth/registration/",include("dj_rest_auth.registration.urls")),
-    path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
-    path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"), 
-    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(
-    url_name="schema"), name="swagger-ui"),
-]
+    path('auth/',include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
