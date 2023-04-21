7efa33db8530e4675a5e5f7c0ae7aa2fcdc2a28c
chidera6 <dictasteil@gmail.com>
2023-04-21 12:18:29 +0100

    feat:added applicants serializer

---
+++ applicantDRF/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import ApplicantProfile
+
+class ApplicantProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplicantProfile
+        fields = '__all__'
