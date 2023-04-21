76d9e7ef848f322666e8e9c2f82ad83839aa964f
chidera6 <dictasteil@gmail.com>
2023-04-21 03:51:05 +0100

    feat:wrote views to post a job,delete a job as well as create a profile

--- employerDRF/views.py
+++ employerDRF/views.py
@@ -10,19 +10,26 @@ class EmployerProfileView(generics.ListCreateAPIView):
     queryset = EmployerProfile.objects.all()
     serializer_class = EmployerProfileSerializer
 
+    def get_queryset(self):
+        return EmployerProfile.objects.all().select_related('user')
 
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+    
 class EmployerProfileSingleView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = EmployerProfile.objects.all()
+    queryset = EmployerProfile.objects.all().select_related('user')
     serializer_class = EmployerProfileSerializer
 
 class PostJobView(generics.ListCreateAPIView):
     queryset = PostJobs.objects.all()
     serializer_class = PostJobSerializer
+    ordering_fields = ['date_created']
+    search_fields = ['experience_level','job_location','date_created']
 
     def perform_create(self, serializer):
         serializer.save(employer=self.request.user.employer_profile)
 
-
 class PostJobSingleView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = PostJobs.objects.all()
+    queryset = PostJobs.objects.all().select_related('employer_profile')
     serializer_class = PostJobSerializer
